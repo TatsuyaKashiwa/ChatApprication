bb3d828bb1fec4dbdb39ea6979f4f097ef62283c
user\TatsuyaKashiwa <TKashi0@outlook.jp>
2024-11-25 14:01:02 +0900

    feat: ServiceDefinitionプロジェクト作成

---
+++ CatApplication.ServiceDefinition/IChatService.cs
@@ -0,0 +1,12 @@
+﻿using MagicOnion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication.ServiceDefinition;
+internal interface IChatService
+{ 
+}
