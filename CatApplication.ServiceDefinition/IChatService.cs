ff1ea7051c6cc4e80f845928205c2a40cd3fd27b
user\TatsuyaKashiwa <TKashi0@outlook.jp>
2024-11-25 15:55:14 +0900

    feat : コメント投稿メソッド実装

--- CatApplication.ServiceDefinition/IChatService.cs
+++ CatApplication.ServiceDefinition/IChatService.cs
@@ -1,16 +1,16 @@
-﻿using MagicOnion;
+﻿using Grpc.Core;
+using MagicOnion;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace ChatApplication.ServiceDefinition;
 public interface IChatService : IService<IChatService>
 { 
-    public void PostComment(HttpContent sessionId,string comment);
+    public void PostComment(ServerCallContext clientContext,string comment);
 
     public UnaryResult<List<string>> ShowCommentArchive();
 }
--- ChatApprication/ChatService.cs
+++ ChatApprication/ChatService.cs
@@ -1,5 +1,16 @@
-﻿namespace ChatApprication.Service;
+﻿using ChatApplication.ServiceDefinition;
+using Grpc.Core;
+using MagicOnion.Server;
 
-public class Class
+namespace ChatApprication.Service;
+
+public class ChatService : ServiceBase<IChatService>, IChatService
 {
+    private List<HttpContext> _commentList = new();
+    public void PostComment(ServerCallContext sessionId, string comment)
+    {
+        var httpContext = sessionId.GetHttpContext();
+        httpContext.Session.SetString($"{sessionId.ToString}", comment);
+        _commentList.Add(httpContext);
+    }
 }
