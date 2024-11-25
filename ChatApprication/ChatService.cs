d1cc1466fb2b6df282c86dd9c4c8668750e2c819
user\TatsuyaKashiwa <TKashi0@outlook.jp>
2024-11-25 16:12:14 +0900

    update : 11/25終了時

--- ChatApprication/ChatService.cs
+++ ChatApprication/ChatService.cs
@@ -6,11 +6,18 @@ namespace ChatApprication.Service;
 
 public class ChatService : ServiceBase<IChatService>, IChatService
 {
+    //HttpContextを使うなら別なstaticな変数に保持すること
+    //Listであればpairを用いる or ディクショナリにタイムスタンプを含める。
     private List<HttpContext> _commentList = new();
-    public void PostComment(ServerCallContext sessionId, string comment)
+    public void PostComment(ServerCallContext clientContext, string comment)
     {
-        var httpContext = sessionId.GetHttpContext();
-        httpContext.Session.SetString($"{sessionId.ToString}", comment);
+        var httpContext = clientContext.GetHttpContext();
+        httpContext.Session.SetString($"{clientContext.ToString}", comment);
         _commentList.Add(httpContext);
+    }
+
+    public List<string> ShowCommentArchive() 
+    {
+        
     }
 }
