dbec930c09c2a90e857bb2f00237df204a098612
user\TatsuyaKashiwa <TKashi0@outlook.jp>
2024-11-27 13:47:39 +0900

    test  : lockerをtry節の中に→try節の中に入っていかないから

--- ChatApprication/ChatService.cs
+++ ChatApprication/ChatService.cs
@@ -16,27 +16,50 @@ public class ChatService : ServiceBase<IChatService>, IChatService
     private static ReaderWriterLockSlim _locker = new ReaderWriterLockSlim();
     static List<CommentClient> comments = new();
 
+    ~ChatService() 
+    {
+        _locker.Dispose();
+    }
+
     public async Task<ClientStreamingResult<string, bool>> SaveAndShowCommentAsync()
     {
-        //[TODO]型を確認すること！
         var streaming = this.GetClientStreamingContext<string, bool>();
+
         var id = streaming.GetHashCode();
-        await streaming.ForEachAsync(x =>
+        try
         {
-            var idAndCommnet = new CommentClient() { SessionID = id, Comment = $"{id}さん ; {x}" };
-            comments.Add(idAndCommnet);
-        });
+            _locker.EnterWriteLock();
+            await streaming.ForEachAsync(x =>
+            {
+                var idAndCommnet = new CommentClient() { SessionID = id, Comment = $"{id}さん ; {x}" };
+                comments.Add(idAndCommnet);
+            });
+        }
+        finally 
+        {
+            _locker.ExitWriteLock();
+        }
 
         return streaming.Result(false);
     }
 
-    //streaming.Result(returnComments)
+
 
     public async UnaryResult<List<string>> GetArchiveAsync() 
     {
-        return comments
+        List<string> returnComments;
+        try
+        {
+            _locker.EnterReadLock();
+            returnComments = comments
             .Select(x => x.Comment)
             .ToList();
+        }
+        finally 
+        { 
+            _locker.ExitReadLock();
+        }
+        return returnComments;
     }
 
 }
