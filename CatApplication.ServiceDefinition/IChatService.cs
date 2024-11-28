6af260ce8a71d697096b74440a221a3892ebf5fe
user\TatsuyaKashiwa <TKashi0@outlook.jp>
2024-11-28 13:27:13 +0900

    update : コメント付け終了

--- CatApplication.ServiceDefinition/IChatService.cs
+++ CatApplication.ServiceDefinition/IChatService.cs
@@ -3,7 +3,9 @@
 namespace ChatApplication.ServiceDefinition;
 public interface IChatService : IService<IChatService>
 {
+    //ClientStreamingならびにコメント保管
     public Task<ClientStreamingResult<string, bool>> SaveCommentAsync();
 
+    //コメント履歴表示
     public UnaryResult<List<string>> GetArchiveAsync();
 }
--- ChatApprication/ChatService.cs
+++ ChatApprication/ChatService.cs
@@ -4,16 +4,36 @@ using MagicOnion.Server;
 
 namespace ChatApprication.Service;
 
+/// <summary>
+/// idとコメントを紐づけるrecord型
+/// </summary>
+/// <remarks>
+/// ClientId : クライアントID
+/// Comment ： 投稿コメント
+/// </remarks>
 public record CommentClient
 {
-    public required int SessionID { get; init; }
+    public required int ClientId { get; init; }
     public required string Comment { get; init; }
 }
 
 public class ChatService : ServiceBase<IChatService>, IChatService
 {
+    /// <summary>
+    /// ユーザID
+    /// </summary>
+    /// <remarks>
+    /// ClientStreamの接続ごとに各クライアントでローカル変数として保持する
+    /// </remarks>
     private static int _id = 0;
 
+    /// <summary>
+    /// lockのためのObject
+    /// </summary>
+    /// <remarks>
+    /// lock用Objectのインスタンスはlockが必要になったときに用いられるため
+    /// Lazy Objectとした。
+    /// </remarks>
     private Lazy<Object> _Locker = new Lazy<Object>(() =>new Object());
 
     /// <summary>
@@ -21,17 +41,28 @@ public class ChatService : ServiceBase<IChatService>, IChatService
     /// </summary>
     /// <remarks>
     /// すべてのクライアントからのコメントを保存するためstaticなフィールドとした
-    /// 
-    /// 初期化は一回のみとしたいため,LazyThreadSafetyModeをExecutionAndPublicationとした
     /// </remarks>
     private static List<CommentClient> _comments = new List<CommentClient>();
 
+    /// <summary>
+    /// ClientStream通信を行うクラス
+    /// </summary>
+    /// <returns>
+    /// false(クライアント側の無限ループを止める)
+    /// </returns>
+    /// <remarks>
+    /// ClientStream通信を行うクラス
+    /// ClientStreamで接続されている間クライアントからの呼び出しで
+    /// コメントの保存を行う
+    /// クライアントから終了の指示が送られると returnが実行されて接続を切断する。
+    /// </remarks>
     public async Task<ClientStreamingResult<string, bool>> SaveCommentAsync()
     {
         //context取得からForEachAsyncの上までの領域は最初にSaveCommentAsyncがクライアントから呼ばれたときに一度だけ呼ばれる
         var context = this.GetClientStreamingContext<string, bool>();
 
-        //この領域で_idをローカル変数に取り込むことでClientStream通信に固有のidを取得することができる
+        //この領域で_idをローカル変数に取り込むことでアクセスしたクライアントのClientStream通信のidを取得する
+        //取得後にインクリメントすることで各クライアントで固有の値とする
         var id = _id;
         _id++;
 
@@ -40,7 +71,7 @@ public class ChatService : ServiceBase<IChatService>, IChatService
         //staticなListへアクセスする際にはlockで排他制御を行う
         await context.ForEachAsync(x =>
         {
-            var idAndCommnet = new CommentClient() { SessionID = id, Comment = $"{id}さん ; {x}" };
+            var idAndCommnet = new CommentClient() { ClientId = id, Comment = $"{id}さん ; {x}" };
             lock (_Locker.Value)
             {
             _comments.Add(idAndCommnet);
@@ -52,8 +83,15 @@ public class ChatService : ServiceBase<IChatService>, IChatService
         return context.Result(false);
     }
 
-
-
+    /// <summary>
+    /// コメント履歴返却
+    /// </summary>
+    /// <returns>
+    /// List<string>型のコメント履歴
+    /// </returns>
+    ///<remarks>
+    ///id・コメントが保存されているListからコメントのみを取り出しクライアントへ返却する
+    ///</remarks>
     public async UnaryResult<List<string>> GetArchiveAsync()
     {
         lock (_Locker.Value)
