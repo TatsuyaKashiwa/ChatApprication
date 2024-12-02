6950c2f8bb408da180265e33b8d3d9ab2b79e2ea
user\TatsuyaKashiwa <TKashi0@outlook.jp>
2024-12-02 09:50:39 +0900

    update : コメントを添付

--- ChatApplication.Client/Program.cs
+++ ChatApplication.Client/Program.cs
@@ -7,6 +7,14 @@ using System.Xml.Linq;
 
 namespace ChatApplication.Client;
 
+/// <summary>
+/// 入力コマンドによる指示の分岐を表すための列挙型
+/// </summary>
+/// <remarks>
+/// DistinguishEntry()拡張メソッドから返却される入力コマンドによる指示を表すための列挙型
+/// 使用されるのはクライアント内(サーバへのアクセスは分岐後)のため
+/// クライアント名前空間内で宣言
+/// </remarks>
 public enum Directions
 {
     Archive,
@@ -41,6 +49,7 @@ internal class Program
         //ストリーム(ClientStreamingResult)作成
         var streaming = await client.SaveCommentAsync();
 
+        //入力受付前にHelpを一度表示
         Console.WriteLine("""
                               コメントを入力してください。
                               -a | --archive : 履歴を表示させたい場合
@@ -52,14 +61,16 @@ internal class Program
         //サーバからClientStreamからのresponseが返ってくるとfalseになりループが終了する
         var canContinue = true;
 
+       //サーバからfalseが返る(終了コマンドが入力される)まで
         while (canContinue)
         {
-            //コメントと履歴表示・終了指示を標準入力から受ける
+            //コメントならびに入力コマンドを標準入力から受ける
             var description = Console.ReadLine();
 
+            //入力に対する指示を受ける
             var direction = description.DistinguishEntry();
 
-            //"archive"が入力されると履歴表示
+            //"-a or --archive"が入力されると履歴表示
             if (direction is Directions.Archive)
             {
                 var comments = await client.GetArchiveAsync();
@@ -68,7 +79,7 @@ internal class Program
                     Console.WriteLine(comment);
                 }
             }
-            //"finish"が入力されると終了処理
+            //"-f or --finish"が入力されると終了処理
             //サーバのForEachAsyncを止めて、返り値(false)をcanContinueに受け取りTCPコネクションを切断
             else if (direction is Directions.Finish)
             {
@@ -76,6 +87,7 @@ internal class Program
                 canContinue = await streaming.ResponseAsync;
                 Console.WriteLine("終了しました");
             }
+            //-h or --helpが入力されるとヘルプを表示
             else if (direction is Directions.Help) 
             {
                 Console.WriteLine("""
